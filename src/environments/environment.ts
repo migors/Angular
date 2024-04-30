45c96fb292e88d8ae651e4332d1ecab3a70af125
migors <igors@seos.lv>
2024-04-30 11:04:47 +0000

    First commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+};
+
+export const Config = {
+  api_key: '',
+  delivery_token: '',
+  environment: '',
+  region: '',
+  management_token: '',
+  api_host: '',
+  app_host: '',
+  live_preview: true,
+};
